1e9e44b932303c8bbfbc65035e10fac89a712de1
George Salu <george.salu10@gmail.com>
2018-07-22 16:16:37 -0300

    incluindo os DAOs especificos

---
+++ src/main/java/com/curso/boot/dao/DapartamentoDaoImpl.java
@@ -0,0 +1,10 @@
+package com.curso.boot.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.curso.boot.domain.Departamento;
+
+@Repository
+public class DapartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao {
+
+}
