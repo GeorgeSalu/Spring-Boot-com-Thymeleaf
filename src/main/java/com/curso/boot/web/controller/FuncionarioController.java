4e58d6f285bc75aaae3e1814c7d4420f0c254033
George Salu <george.salu10@gmail.com>
2018-07-28 11:55:38 -0300

    editando e excluindo funcionario

--- src/main/java/com/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/curso/boot/web/controller/FuncionarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -54,4 +55,24 @@ public class FuncionarioController {
 	public UF[] getUfs() {
 		return UF.values();
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscarPorId(id));
+		return "funcionario/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}	
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		funcionarioService.excluir(id);
+		attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
+		return "redirect:/funcionarios/listar";
+	}	
 }
